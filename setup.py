af6d79ddeee32d6204468257979dd55f30eb4396
Mati Green <mati.green.13@gmail.com>
2019-05-31 21:24:58 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="vigram",
+    version="0.0.1",
+    author="Mati Green",
+    author_email="mati.green.13@gmail.com",
+    description="Python Telegram client writed with matiui",
+    url="https://github.com/GMati13/vi-telegram",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: Unix/Linux like system",
+    ],
+)
